1174f97b3d7d7c5c8af7c3fd2ebd3cf1a781c7fc
ZakolaczekHWDP <zakolakpremium@gmail.com>
2025-09-05 18:29:30 +0200

    # New method!

---
+++ MethodSystem/Methods/PlayerMethods/SetGroupMethod.cs
@@ -0,0 +1,27 @@
+﻿using LabApi.Features.Wrappers;
+using SER.MethodSystem.ArgumentSystem.Arguments;
+using SER.MethodSystem.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+using System.Collections.Generic;
+
+namespace SER.MethodSystem.Methods.PlayerMethods;
+public class SetGroupMethod : SynchronousMethod
+{
+    public override string Description => "Sets or removes group for provided players";
+
+    public override GenericMethodArgument[] ExpectedArguments =>
+    [
+        new PlayersArgument("players"),
+        new TextArgument("group")
+        {
+            Description = "Set to NONE if you want to remove the group!",
+        }
+    ];
+
+    public override void Execute()
+    {
+        List<Player> pls = Args.GetPlayers("players");
+        string group = Args.GetText("group");
+        pls.ForEach(p => p.UserGroup = ServerStatic.PermissionsHandler.GetGroup(group != "NONE" ? group : null));
+    }
+}
