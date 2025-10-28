3b1bc63ea4769e3622566f6c26e5da169e67f23d
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-10-28 14:43:29 +0100

    added StartRound and EndRound methods

---
+++ MethodSystem/Methods/RoundMethods/EndRoundMethod.cs
@@ -0,0 +1,17 @@
+﻿using LabApi.Features.Wrappers;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+
+namespace SER.MethodSystem.Methods.RoundMethods;
+
+public class EndRoundMethod : SynchronousMethod
+{
+    public override string? Description => "Ends a round.";
+
+    public override Argument[] ExpectedArguments { get; } = [];
+    
+    public override void Execute()
+    {
+        Round.End(true);
+    }
+}
