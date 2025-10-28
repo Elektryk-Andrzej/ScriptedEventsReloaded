4fc87d759c7ff927b0951a8782d41a558ea3cfd4
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-10-28 12:48:03 +0100

    fix enums again

--- ArgumentSystem/Arguments/EnumArgument.cs
+++ ArgumentSystem/Arguments/EnumArgument.cs
@@ -53,7 +53,8 @@ public class EnumArgument<TEnum> : Argument where TEnum : struct, Enum
         var stringRep = token.GetBestTextRepresentation(script);
         
         // only allow exact matches or matches with the first letter not capitalized
-        if (Enum.IsDefined(enumType, stringRep))
+        if (Enum.IsDefined(enumType, stringRep) || 
+            Enum.GetNames(enumType).Any(n => n.LowerFirst() == stringRep))
         {
             return Enum.Parse(enumType, stringRep, true);
         }
