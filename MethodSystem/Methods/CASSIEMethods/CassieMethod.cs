9e6a83fe50a649ee869142a43372d04e9ca31a2e
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-10-29 08:52:10 +0100

    added glitch argument to cassie

--- MethodSystem/Methods/CASSIEMethods/CassieMethod.cs
+++ MethodSystem/Methods/CASSIEMethods/CassieMethod.cs
@@ -1,6 +1,9 @@
-﻿using Respawning;
+﻿using System;
+using System.Linq;
+using Respawning;
 using SER.ArgumentSystem.Arguments;
 using SER.ArgumentSystem.BaseArguments;
+using SER.Helpers.Extensions;
 using SER.MethodSystem.BaseMethods;
 
 namespace SER.MethodSystem.Methods.CASSIEMethods;
@@ -19,6 +22,11 @@ public class CassieMethod : SynchronousMethod
         new TextArgument("translation")
         {
             DefaultValue = "",
+        },
+        new BoolArgument("should glitch")
+        {
+            Description = "If true, SER will add random glitch effects to the announcement.",
+            DefaultValue = false,
         }
     ];
     
@@ -27,12 +35,38 @@ public class CassieMethod : SynchronousMethod
         var isNoisy = Args.GetOption("mode") == "jingle";
         var message = Args.GetText("message");
         var translation = Args.GetText("translation");
-        
-        RespawnEffectsController.PlayCassieAnnouncement(
-            message, 
-            false, 
-            isNoisy, 
-            !string.IsNullOrWhiteSpace(translation),
-            translation);
+        var glitch = Args.GetBool("should glitch");
+
+        if (glitch)
+        {
+            message = "jam_100_9 " + message
+                .Split([' '], StringSplitOptions.RemoveEmptyEntries)
+                .Select(part =>
+                {
+                    if (UnityEngine.Random.Range(1, 3) == 1) return part;
+                    return $"pitch_{UnityEngine.Random.Range(0.7f, 1.2f)} {part}";
+
+                })
+                .JoinStrings(" ");
+        }
+
+        if (string.IsNullOrEmpty(translation))
+        {
+            RespawnEffectsController.PlayCassieAnnouncement(
+                message, 
+                false, 
+                isNoisy
+            );
+        }
+        else
+        {
+            RespawnEffectsController.PlayCassieAnnouncement(
+                message, 
+                false, 
+                isNoisy, 
+                true,
+                translation
+            );
+        }
     }
 }
