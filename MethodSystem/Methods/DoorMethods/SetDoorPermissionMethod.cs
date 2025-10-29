80ea354f5d0b03818c0fd16d9d1bd1b51c4eeb06
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-10-29 08:51:08 +0100

    add a glitch argument to Cassie method

---
+++ MethodSystem/Methods/DoorMethods/SetDoorPermissionMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Interactables.Interobjects.DoorUtils;
+using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+
+namespace SER.MethodSystem.Methods.DoorMethods;
+
+public class SetDoorPermissionMethod : SynchronousMethod
+{
+    public override string Description => "Sets door permissions.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new DoorsArgument("doors"),
+        new EnumArgument<DoorPermissionFlags>("permissions")
+        {
+            ConsumesRemainingValues = true,
+        }
+    ];
+    
+    public override void Execute()
+    {
+        var doors = Args.GetDoors("doors");
+        var permissions = Args
+            .GetRemainingArguments<object, EnumArgument<DoorPermissionFlags>>("permissions")
+            .Cast<DoorPermissionFlags>()
+            .ToArray();
+        
+        doors.ForEach(door =>
+        {
+            door.Permissions = permissions.Aggregate((a, b) => a | b);
+        });
+    }
+}
