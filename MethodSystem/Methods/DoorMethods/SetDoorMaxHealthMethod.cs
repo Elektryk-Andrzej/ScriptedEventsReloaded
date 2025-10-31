93644ab9f7b6aad623d6f389ed65c8611323e126
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-10-31 10:00:45 +0100

    Update SetDoorMaxHealthMethod.cs

--- MethodSystem/Methods/DoorMethods/SetDoorMaxHealthMethod.cs
+++ MethodSystem/Methods/DoorMethods/SetDoorMaxHealthMethod.cs
@@ -1,10 +1,10 @@
-﻿using Interactables.Interobjects.DoorUtils;
-using LabApi.Features.Wrappers;
+﻿using LabApi.Features.Wrappers;
 using SER.ArgumentSystem.Arguments;
 using SER.ArgumentSystem.BaseArguments;
 using SER.MethodSystem.BaseMethods;
 
 namespace SER.MethodSystem.Methods.DoorMethods;
+
 public class SetDoorMaxHealthMethod : SynchronousMethod
 {
     public override string Description => "Sets max health for specified doors if possible";
@@ -12,19 +12,14 @@ public class SetDoorMaxHealthMethod : SynchronousMethod
     public override Argument[] ExpectedArguments =>
     [
         new DoorsArgument("doors"),
-        new FloatArgument("maxhealth")
+        new FloatArgument("max health")
     ];
 
     public override void Execute()
     {
-        float MaxHP = Args.GetFloat("maxhealth");
         Door[] doors = Args.GetDoors("doors");
-        foreach (Door door in doors)
-        {
-            if (door is BreakableDoor brek)
-            {
-                brek.MaxHealth = MaxHP;
-            }
-        }
+        float maxHp = Args.GetFloat("max health");
+        
+        doors.OfType<Door, BreakableDoor>().ForEach(door => door.MaxHealth = maxHp);
     }
 }
