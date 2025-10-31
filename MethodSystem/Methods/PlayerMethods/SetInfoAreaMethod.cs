84518d845c6b798714ad77591b1d44a3a9a1d51a
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-10-31 10:03:01 +0100

    Update SetInfoAreaMethod.cs

--- MethodSystem/Methods/PlayerMethods/SetInfoAreaMethod.cs
+++ MethodSystem/Methods/PlayerMethods/SetInfoAreaMethod.cs
@@ -5,25 +5,25 @@ using SER.MethodSystem.BaseMethods;
 using System.Collections.Generic;
 
 namespace SER.MethodSystem.Methods.PlayerMethods;
+
 public class SetInfoAreaMethod : SynchronousMethod
 {
     public override string Description => "Sets InfoArea for specified players";
 
-    public override Argument[] ExpectedArguments => [
+    public override Argument[] ExpectedArguments => 
+    [
         new PlayersArgument("players"),
-        new EnumArgument<PlayerInfoArea>("infoarea")
+        new EnumArgument<PlayerInfoArea>("info area")
         {
             ConsumesRemainingValues = true,
         }
-        ];
+    ];
 
     public override void Execute()
     {
-        List<Player> pls = Args.GetPlayers("players");
-        PlayerInfoArea area = Args.GetEnum<PlayerInfoArea>("infoarea");
-        foreach(Player p in pls)
-        {
-            p.InfoArea = area;
-        }
+        List<Player> players = Args.GetPlayers("players");
+        PlayerInfoArea info = Args.GetEnum<PlayerInfoArea>("info area");
+        
+        players.ForEach(p => p.InfoArea = info);
     }
 }
